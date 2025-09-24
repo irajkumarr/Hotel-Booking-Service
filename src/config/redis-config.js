f4c5faa2479f7813074a699b3756b9739e9f94d1
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-09-24 18:14:34 +0545

    configure the redis and redlock issue

--- src/config/redis-config.js
+++ src/config/redis-config.js
@@ -1,6 +1,8 @@
+// @ts-ignore
 const IORedis = require("ioredis");
 const serverConfig = require("./server-config");
 const logger = require("./logger-config");
+const Redlock = require("redlock");
 
 // Redis connection
 const redisConnection = new IORedis(serverConfig.REDIS_URL, {
@@ -15,4 +17,11 @@ redisConnection.on("error", (err) => {
   logger.error("❌ Redis connection error:", err.message);
 });
 
-module.exports = { redisConnection };
+const redlock = new Redlock([redisConnection], {
+  driftFactor: 0.01, // time in ms
+  retryCount: 10,
+  retryDelay: 200, // time in ms
+  retryJitter: 200, // time in ms
+});
+
+module.exports = { redisConnection, redlock };
